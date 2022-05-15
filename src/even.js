fd9f2cc24314e2c07fb1acf5d5ba0adb53a8c63a
Колосов Станислав <kolosov.stas01@mail.ru>
2022-05-15 23:37:13 +1000

    add new file

--- src/even.js
+++ src/even.js
@@ -8,15 +8,14 @@ const game = () => {
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   const gameRounds = 3;
-  for (let i = 0; i < gameRounds; i++) {
+  for (let i = 0; i < gameRounds; i += 1) {
     const question = numberGenerator(1, 100);
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('You answer: ');
     if (question % 2 === 0) {
       if (userAnswer === 'yes') {
         console.log('Correct!');
-      }
-      else {
+      } else {
         console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'yes'.`);
         console.log(`Let's try again, ${userName}!`);
         return;
@@ -25,8 +24,7 @@ const game = () => {
     if (question % 2 !== 0) {
       if (userAnswer === 'no') {
         console.log('Correct!');
-      }
-      else {
+      } else {
         console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.`);
         console.log(`Let's try again, ${userName}!`);
         return;
