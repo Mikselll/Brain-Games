0e231f873ed333161786bfbd6cd08b5d1cfdc413
Колосов Станислав <kolosov.stas01@mail.ru>
2022-05-15 22:25:00 +1000

    add new files

---
+++ src/even.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+
+const numberGenerator = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const game = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const gameRounds = 3;
+  for (let i = 0; i < gameRounds; i++) {
+    const question = numberGenerator(1, 100);
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('You answer: ');
+    if (question % 2 === 0) {
+      if (userAnswer === 'yes') {
+        console.log('Correct!');
+      }
+      else {
+        console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'yes'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
+      }
+    }
+    if (question % 2 !== 0) {
+      if (userAnswer === 'no') {
+        console.log('Correct!');
+      }
+      else {
+        console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
+      }
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default game;
