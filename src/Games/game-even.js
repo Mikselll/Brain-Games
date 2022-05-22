43349c7e19bd411a30cf646dc840b57f3f61d3e3
Колосов Станислав <kolosov.stas01@mail.ru>
2022-05-22 14:48:51 +1000

    add fix gameEven

--- src/Games/game-even.js
+++ src/Games/game-even.js
@@ -2,7 +2,7 @@ import startGame from '../index.js';
 
 const numberGenerator = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-const checkEven = (num) => num % 2 ===0;
+const checkEven = (num) => num % 2 === 0;
 
 const evenRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
