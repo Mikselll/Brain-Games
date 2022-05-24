3247b1872e03d9788c83c06aee095a161761b65e
Колосов Станислав <kolosov.stas01@mail.ru>
2022-05-24 23:11:25 +1000

    fix gameProgression

--- src/Games/game-progression.js
+++ src/Games/game-progression.js
@@ -1,30 +1,25 @@
 import startGame from '../index.js';
-
-const numberGenerator = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-const getRandomStep = numberGenerator(0, 9);
+import numberGenerator from '../utils.js';
 
 const createProgression = () => {
   const progression = [];
   progression.push(numberGenerator(1, 100));
   const step = numberGenerator(1, 10);
-  for (let i = 0; i <= 9; i += 1) {
+  const progressionLength = 9;
+  for (let i = 0; i <= progressionLength; i += 1) {
     progression.push(progression[i] + step);
   }
   return progression;
 };
 
-const getQuestion = (array) => {
-  array.splice(getRandomStep, 1, '..');
-  return array;
-};
-
 const progressionRule = 'What number is missing in the progression?';
 
 const progressionStep = () => {
+  const hiddenNumberIndex = numberGenerator(0, 9);
   const progression = createProgression();
-  const rightAnswer = String(progression[getRandomStep]);
-  const gameQuestion = `${getQuestion(progression).join(' ')}`;
+  const rightAnswer = String(progression[hiddenNumberIndex]);
+  progression.splice(hiddenNumberIndex, 1, '..');
+  const gameQuestion = `${progression.join(' ')}`;
   return [gameQuestion, rightAnswer];
 };
 
