39acead27221466c4681ce3a9be7369f1a11b1f0
Колосов Станислав <kolosov.stas01@mail.ru>
2022-05-24 23:26:43 +1000

    add rundomNumber

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,3 @@
-const numberGenerator = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
+const numberGenerator = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 export default numberGenerator;
