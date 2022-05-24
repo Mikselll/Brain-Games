ff2a3641ce26f4cc9a496adf6e037adb80d1ffc0
Колосов Станислав <kolosov.stas01@mail.ru>
2022-05-24 23:09:36 +1000

    fix gameCalc

--- src/Games/game-calc.js
+++ src/Games/game-calc.js
@@ -1,8 +1,7 @@
 import startGame from '../index.js';
+import numberGenerator from '../utils.js';
 
-const numberGenerator = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-const arrOperators = ['+', '-', '*'];
+const operators = ['+', '-', '*'];
 
 const getRightAnswer = (a, b, operator) => {
   if (operator === '+') {
@@ -22,7 +21,7 @@ const calcRule = 'What is the result of the expression?';
 const calcStep = () => {
   const firstRandomNumber = numberGenerator(1, 10);
   const secondRandomNumber = numberGenerator(1, 10);
-  const randomOperator = arrOperators[numberGenerator(0, arrOperators.length - 1)];
+  const randomOperator = operators[numberGenerator(0, operators.length - 1)];
   const gameQuestion = `${firstRandomNumber} ${randomOperator} ${secondRandomNumber}`;
   const rightAnswer = String(getRightAnswer(firstRandomNumber, secondRandomNumber, randomOperator));
   return [gameQuestion, rightAnswer];
